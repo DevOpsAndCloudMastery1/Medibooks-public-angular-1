c4d08988ac9aa8f0bfb83f3d5e5f243fab4fd916
dhanya <dhanyadhilip191@gmail.com>
2025-03-20 12:27:51 +0100

    updated path and import in app.routes.ts for doctor-details component

--- projects/ng-learning/project-1/src/app/app.routes.ts
+++ projects/ng-learning/project-1/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DoctorSearchComponent } from './doctor-search/doctor-search.component'; 
+import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
 import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'doctor-search', component: DoctorSearchComponent },
+  { path: 'doctor-details/:id', component: DoctorDetailsComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   ];
--- projects/ng-learning/project-1/src/app/doctor-details/doctor-details.component.spec.ts
+++ projects/ng-learning/project-1/src/app/doctor-details/doctor-details.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';  // Import of
 import { DoctorDetailsComponent } from './doctor-details.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 describe('DoctorDetailsComponent', () => {
   let component: DoctorDetailsComponent;
   let fixture: ComponentFixture<DoctorDetailsComponent>;
+  let httpTestingController: HttpTestingController;
+
+  const mockActivatedRoute = {
+    params: of({ id: '1' })  // Mock the route params
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DoctorDetailsComponent]
+      imports: [CommonModule,HttpClientTestingModule,RouterModule,DoctorDetailsComponent]
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(DoctorDetailsComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch doctor details on init', () => {
+    const mockDoctorData = {
+      doctors: [
+        {
+          id: '1',
+          name: 'Dr. Test',
+          img: 'test.jpg',
+          specialization: 'Test',
+          experience: '10 years',
+          location: 'Test Location',
+          description: 'Test Description'
+        }
+      ]
+    };
+
+    const req = httpTestingController.expectOne('data/doctors.json');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockDoctorData);
+
+    expect(component.doctor).toBeDefined();
+    expect(component.doctor?.name).toEqual('Dr. Test');
+  });
+    afterEach(() => {
+        httpTestingController.verify();
+    });
 });
