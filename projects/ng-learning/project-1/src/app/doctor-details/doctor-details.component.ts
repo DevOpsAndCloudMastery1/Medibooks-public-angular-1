0e30ddd94cc76e738ed6557db4794e276879d8a1
dhanya <dhanyadhilip191@gmail.com>
2025-03-20 12:07:16 +0100

    feat: Add Doctor Details Page
    This commit creates a page to display detailed information about a specific doctor, fetched from a JSON file based on the ID in the URL.

--- projects/ng-learning/project-1/src/app/doctor-details/doctor-details.component.ts
+++ projects/ng-learning/project-1/src/app/doctor-details/doctor-details.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface Doctor {
+  id: string;
+  name: string;
+  img: string;
+  specialization: string;
+  experience: string;
+  location: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-doctor-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './doctor-details.component.html',
   styleUrl: './doctor-details.component.css'
 })
-export class DoctorDetailsComponent {
+export class DoctorDetailsComponent  implements OnInit {
+  doctorId: string | null = null;
+  doctor: Doctor | undefined;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.doctorId = params['id'];
+
+      if (this.doctorId) {
+        this.http.get<{ doctors: Doctor[] }>('data/doctors.json')
+          .subscribe(data => {
+            this.doctor = data.doctors.find(doc => doc.id === this.doctorId);
+          });
+      }
+    });
+  }
 
 }
