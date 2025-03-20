33e2e72b6c6ad8ee7a21ad3ada1dda832c54f6f2
dhanya <dhanyadhilip191@gmail.com>
2025-03-20 22:06:53 +0100

    feat: Add standalone AppointmentBookingComponent with doctor selection and appointment booking functionality in appointment-booking.component.ts

--- projects/ng-learning/project-1/src/app/appointment-booking/appointment-booking.component.ts
+++ projects/ng-learning/project-1/src/app/appointment-booking/appointment-booking.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+interface Doctor {
+  id: string;
+  name: string;
+  specialization: string;
+  experience: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-appointment-booking',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './appointment-booking.component.html',
   styleUrl: './appointment-booking.component.css'
 })
-export class AppointmentBookingComponent {
+export class AppointmentBookingComponent implements OnInit {
+  doctorId: string | null = null;
+  doctor: Doctor | undefined;
+  appointmentConfirmed = false;
+  appointmentData = {
+    date: '',
+    time: '',
+    name: '',
+    email: '',
+    phone: ''
+  };
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.doctorId = params['id'];
+      if (this.doctorId) {
+        this.http.get<{ doctors: Doctor[] }>('data/doctors.json').subscribe(data => {
+          this.doctor = data.doctors.find(doc => doc.id === this.doctorId);
+        });
+      }
+    });
+  }
+
+  bookAppointment(): void {
+    this.appointmentConfirmed = true;
+  }
 }
