216f8731411118ff8c40d5135e68d0f4054ade09
dhanya <dhanyadhilip191@gmail.com>
2025-03-20 11:40:18 +0100

    added import and export path to app.routes.ts for doctor-search component

--- projects/ng-learning/project-1/src/app/app.routes.ts
+++ projects/ng-learning/project-1/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DoctorSearchComponent } from './doctor-search/doctor-search.component'; 
 import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'doctor-search', component: DoctorSearchComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   ];
