b20912d6a62738911825115a82c82b0d100f2197
dhanya <dhanyadhilip191@gmail.com>
2025-03-20 22:18:30 +0100

    updated import and export path for appointment-booking in app.routes.ts

--- projects/ng-learning/project-1/src/app/app.routes.ts
+++ projects/ng-learning/project-1/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DoctorSearchComponent } from './doctor-search/doctor-search.component'; 
 import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
+import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'doctor-search', component: DoctorSearchComponent },
   { path: 'doctor-details/:id', component: DoctorDetailsComponent },
+  { path: 'appointment-booking', component: AppointmentBookingComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   ];
