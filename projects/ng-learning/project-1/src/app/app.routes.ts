dd1050a72db4d19271713ff46609270441fbeafb
dhanya <dhanyadhilip191@gmail.com>
2025-03-25 18:07:59 +0100

    update path and import in app.routes.ts file

--- projects/ng-learning/project-1/src/app/app.routes.ts
+++ projects/ng-learning/project-1/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DoctorSearchComponent } from './doctor-search/doctor-search.component';
 import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
 import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 import { AppointmentManagementComponent } from './appointment-management/appointment-management.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'doctor-details/:id', component: DoctorDetailsComponent },
   { path: 'appointment-booking', component: AppointmentBookingComponent },
   { path: 'appointment-management', component: AppointmentManagementComponent },
+  { path: 'user-profile', component: UserProfileComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   ];
