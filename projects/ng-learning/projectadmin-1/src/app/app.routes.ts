134d243adfd7de3b100af967d53b39625d546731
dhanya <dhanyadhilip191@gmail.com>
2025-04-01 15:26:39 +0200

    Added Path and imported managedoctor component in app.routes.ts

--- projects/ng-learning/projectadmin-1/src/app/app.routes.ts
+++ projects/ng-learning/projectadmin-1/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ManageDoctorsComponent } from './manage-doctors/manage-doctors.component';
 
 export const routes: Routes = [
    { path: 'login', component: AdminLoginComponent },
    { path: 'admin-dashboard', component: AdminDashboardComponent },
+   { path: 'manage-doctors', component: ManageDoctorsComponent },
    { path: '', redirectTo: '/login', pathMatch: 'full' },
    ];
