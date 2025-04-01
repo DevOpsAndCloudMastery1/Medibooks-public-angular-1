8622d699cf543f5c759ec1129b06c9d2c5423f57
dhanya <dhanyadhilip191@gmail.com>
2025-04-01 13:48:45 +0200

    added logout function in app.component.ts

--- projects/ng-learning/projectadmin-1/src/app/app.component.ts
+++ projects/ng-learning/projectadmin-1/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Medibooks';
-
+  // Add the logout method
+  logout() {
+    // You can handle the logout logic here, like redirecting or clearing user data
+    console.log("Logged out successfully.");
   }
+}
+
