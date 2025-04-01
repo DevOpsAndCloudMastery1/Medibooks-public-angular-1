c643ef396b7eb475b4df9faff5b201cd173cb0c7
dhanya <dhanyadhilip191@gmail.com>
2025-04-01 13:14:42 +0200

    generated admin-login components

--- projects/ng-learning/projectadmin-1/src/app/app.component.ts
+++ projects/ng-learning/projectadmin-1/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Medibooks';
+
+  logout(): void {
+    console.log('Global Logout clicked');
+    alert('You have logged out!');
+    window.location.href = 'login.html';
+  }
 }
