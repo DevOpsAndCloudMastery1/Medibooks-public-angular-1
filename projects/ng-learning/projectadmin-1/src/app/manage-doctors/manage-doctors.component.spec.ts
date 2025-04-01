0b28c3f233240f0401970c148be444017e71aee5
dhanya <dhanyadhilip191@gmail.com>
2025-04-01 15:24:50 +0200

    test(admin): Add initial tests for ManageDoctorsComponent
    Sets up the basic testing structure for ManageDoctorsComponent using TestBed.
    - Includes necessary imports (RouterTestingModule).
    - Adds a fundamental test case to ensure the component 'should create'.
    - Adds an initial test case for the  method, simulating basic input and checking if the doctors array grows in manage-doctors.component.spec.ts

--- projects/ng-learning/projectadmin-1/src/app/manage-doctors/manage-doctors.component.spec.ts
+++ projects/ng-learning/projectadmin-1/src/app/manage-doctors/manage-doctors.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ManageDoctorsComponent } from './manage-doctors.component';
 
 describe('ManageDoctorsComponent', () => {
@@ -8,10 +8,12 @@ describe('ManageDoctorsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ManageDoctorsComponent]
+      imports: [ManageDoctorsComponent, RouterTestingModule]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ManageDoctorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +22,16 @@ describe('ManageDoctorsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add a doctor', () => {
+    component.doctorName = 'Dr. Test';
+    component.specialization = 'Test Specialist';
+    component.experience = '5 years';
+    component.availability = 'Available';
+    component.photo = new File(['dummy content'], 'test.jpg', { type: 'image/jpeg' });
+
+    component.addDoctor(new Event('submit'));
+
+    expect(component.doctors.length).toBeGreaterThan(0);
+  }); 
 });
