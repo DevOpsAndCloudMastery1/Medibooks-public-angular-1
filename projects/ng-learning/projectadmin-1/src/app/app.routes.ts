09fd08735fb37315342afe056dbb453f04174eb9
dhanya <dhanyadhilip191@gmail.com>
2025-04-01 10:41:21 +0200

    updated path in app.routes.ts

--- projects/ng-learning/projectadmin-1/src/app/app.routes.ts
+++ projects/ng-learning/projectadmin-1/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'admin-dashboard', component: AdminDashboardComponent },
+   { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
