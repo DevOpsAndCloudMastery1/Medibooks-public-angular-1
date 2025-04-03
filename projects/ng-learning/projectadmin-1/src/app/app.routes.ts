f44de7e7796052b76c726aa60743ed3121399c52
dhanya <dhanyadhilip191@gmail.com>
2025-04-03 09:40:43 +0200

    updated path in app.routes.ts

--- projects/ng-learning/projectadmin-1/src/app/app.routes.ts
+++ projects/ng-learning/projectadmin-1/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ManageDoctorsComponent } from './manage-doctors/manage-doctors.component';
+import { ManageAppointmentsComponent } from './manage-appointments/manage-appointments.component';
 
 export const routes: Routes = [
    { path: 'login', component: AdminLoginComponent },
    { path: 'admin-dashboard', component: AdminDashboardComponent },
    { path: 'manage-doctors', component: ManageDoctorsComponent },
+   { path: 'manage-appointments', component: ManageAppointmentsComponent },
    { path: '', redirectTo: '/login', pathMatch: 'full' },
    ];
