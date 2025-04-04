a06a0a626174224c17f0cdb0d9090e9d655494a1
dhanya <dhanyadhilip191@gmail.com>
2025-04-04 09:49:07 +0200

    fix-added routertestingmodule in home.component.spec.ts

--- projects/ng-learning/project-1/src/app/user-profile/user-profile.component.spec.ts
+++ projects/ng-learning/project-1/src/app/user-profile/user-profile.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserProfileComponent } from './user-profile.component';
 
 describe('UserProfileComponent', () => {
@@ -10,7 +9,7 @@ describe('UserProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserProfileComponent, RouterTestingModule, HttpClientTestingModule]
+      imports: [UserProfileComponent, RouterTestingModule]
     })
     .compileComponents();
 
