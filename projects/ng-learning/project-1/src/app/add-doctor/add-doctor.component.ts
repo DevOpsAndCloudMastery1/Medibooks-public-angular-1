3e7f1a561a378b619c90e2d31762df88ac0502fd
dhanya <dhanyadhilip191@gmail.com>
2025-05-28 14:11:56 +0200

    Update AddDoctorComponent to use reactive forms with validation, remove id field, and add form reset on successful submission

--- projects/ng-learning/project-1/src/app/add-doctor/add-doctor.component.ts
+++ projects/ng-learning/project-1/src/app/add-doctor/add-doctor.component.ts
@@ -1,30 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { DoctorService } from '../services/doctor.service';
 
 @Component({
-  standalone: true,
   selector: 'app-add-doctor',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './add-doctor.component.html',
-  styleUrls: ['./add-doctor.component.css]
+  styleUrls: ['./add-doctor.component.css']
 })
 export class AddDoctorComponent {
   doctorForm: FormGroup;
 
   constructor(private fb: FormBuilder, private doctorService: DoctorService) {
     this.doctorForm = this.fb.group({
-      id: [''],
-      name: [''],
-      specialization: [''],
-      location: [''],
-      experience: [''],
-      rating: ['']
+      name: ['', Validators.required],
+      specialization: ['', Validators.required],
+      location: ['', Validators.required],
+      experience: [0, [Validators.required, Validators.min(0)]],
+      rating: [0, [Validators.required, Validators.min(0), Validators.max(5)]]
     });
   }
 
   onSubmit() {
-    this.doctorService.addDoctor(this.doctorForm.value).subscribe(response => {
-      alert('Doctor added successfully!');
-    });
+    if (this.doctorForm.valid) {
+      this.doctorService.addDoctor(this.doctorForm.value).subscribe(response => {
+        alert('Doctor added successfully!');
+        this.doctorForm.reset();
+      });
+    }
   }
 }
