8a3770f3d252e7db7385439affdc231cbf96e6a4
dhanya <dhanyadhilip191@gmail.com>
2025-05-28 14:07:31 +0200

    Add AddDoctorComponent with form for adding new doctors via DoctorService

--- projects/ng-learning/project-1/src/app/add-doctor/add-doctor.component.ts
+++ projects/ng-learning/project-1/src/app/add-doctor/add-doctor.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DoctorService } from '../services/doctor.service';
 
 @Component({
+  standalone: true,
   selector: 'app-add-doctor',
-  imports: [],
   templateUrl: './add-doctor.component.html',
-  styleUrl: './add-doctor.component.css'
+  styleUrls: ['./add-doctor.component.css]
 })
 export class AddDoctorComponent {
+  doctorForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private doctorService: DoctorService) {
+    this.doctorForm = this.fb.group({
+      id: [''],
+      name: [''],
+      specialization: [''],
+      location: [''],
+      experience: [''],
+      rating: ['']
+    });
+  }
+
+  onSubmit() {
+    this.doctorService.addDoctor(this.doctorForm.value).subscribe(response => {
+      alert('Doctor added successfully!');
+    });
+  }
 }
