1437583067fee98b2594020e16214848af373f6b
dhanya <dhanyadhilip191@gmail.com>
2025-06-03 10:41:26 +0200

    added route for edit and list components

--- projects/ng-learning/project-1/src/app/app.routes.ts
+++ projects/ng-learning/project-1/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './login/login.component';
 import { DoctorSearchComponent } from './doctor-search/doctor-search.component'; 
 import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
 import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { DoctorListComponent } from './doctor-list/doctor-list.component';
+import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
 import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 import { AppointmentManagementComponent } from './appointment-management/appointment-management.component';
 import { UserProfileEditComponent } from './user-profile-edit/user-profile-edit.component'; 
@@ -16,6 +18,8 @@ export const routes: Routes = [
   { path: 'doctor-search', component: DoctorSearchComponent },
   { path: 'doctor-details/:id', component: DoctorDetailsComponent },
   { path: 'add-doctor', component: AddDoctorComponent },
+  { path: 'doctor-list', component: DoctorListComponent },
+  { path: 'edit-doctor/:id', component: EditDoctorComponent },
   { path: 'appointment-booking', component: AppointmentBookingComponent },
   { path: 'appointment-management', component: AppointmentManagementComponent },
   { path: 'user-profile', component: UserProfileComponent },
