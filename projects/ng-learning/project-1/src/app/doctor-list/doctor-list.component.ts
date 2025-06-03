ef452f0c0fbe32731e382c9c8d0a7b6a71bca892
dhanya <dhanyadhilip191@gmail.com>
2025-06-03 10:17:29 +0200

    feat: add Doctor List component with view, edit, and delete functionality

--- projects/ng-learning/project-1/src/app/doctor-list/doctor-list.component.ts
+++ projects/ng-learning/project-1/src/app/doctor-list/doctor-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-doctor-list',
-  imports: [],
   templateUrl: './doctor-list.component.html',
-  styleUrl: './doctor-list.component.css'
+  styleUrl: './doctor-list.component.css',
+  imports: [],
 })
-export class DoctorListComponent {
+export class DoctorListComponent implements OnInit {
+  doctors: any[] = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://192.168.0.63:3000/api/doctors').subscribe(data => {
+      this.doctors = data;
+    });
+  }
+
+  editDoctor(id: string): void {
+    this.router.navigate(['/edit-doctor', id]);
+  }
+
+  deleteDoctor(id: string): void {
+    this.http.delete(`http://192.168.0.63:3000/api/doctors/${id}`).subscribe(() => {
+      this.doctors = this.doctors.filter(doc => doc.id !== id);
+    });
+  }
 }
